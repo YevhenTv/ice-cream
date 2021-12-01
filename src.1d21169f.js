6f28634cd5592dcb7fe9d21a358e716ac644907b
VladimirBludov <36624452+VladimirBludov@users.noreply.github.com>
2021-12-01 07:07:03 +0000

    Deploying to gh-pages from @ VladimirBludov/ice-cream@c19f0cd3453a8d3eedc4150b7f7590b7b800a19b 🚀

--- src.1d21169f.js
+++ src.1d21169f.js
@@ -3,4 +3,4 @@ parcelRequire=function(e,r,t,n){var i,o="function"==typeof parcelRequire&&parcel
 },{"./../images/demo.jpg":[["demo.090c7c6d.jpg","nKR3"],"nKR3"]}],"Focm":[function(require,module,exports) {
 "use strict";require("./sass/main.scss");
 },{"./sass/main.scss":"clu1"}]},{},["Focm"], null)
-//# sourceMappingURL=/ice-cream/src.a32d333d.js.map
+//# sourceMappingURL=/ice-cream/src.1d21169f.js.map
